2183b18115451fff9fdf9b5c09eec17246ff64db
WongHoay <wonghoay123@gmail.com>
2024-09-30 13:49:51 +0800

    enterprise project
    # Conflicts:
    #	includes/header2.php

---
+++ userprofile.php
@@ -0,0 +1,40 @@
+<?php
+include 'includes\header.php';
+echo ' | User Profile';
+include 'includes\header2.php';
+include 'includes\navbar.php';
+?>
+<div>
+    <div>
+        <h2>Profile Information</h2>
+    </div>
+    
+    <td>
+        
+    </td>
+    
+    <div>
+        <table>
+            <tr>
+                <td>Username:</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Phone:</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+<?php
+include 'includes\footer.php';
+?>
