a838bb162bb3efe6bf8e1e16ec358a34f34db993
User <User@Mystic-PC>
2024-10-04 12:38:02 +0800

    enterprise project
    # Conflicts:
    #	includes/header2.php
    
    # Conflicts:
    #	includes/footer.php
    #	index.php

--- includes/footer.php
+++ includes/footer.php
@@ -8,21 +8,22 @@
     </body>
 </html>-->
 
-            <footer class="site-footer text-center pt-3 mt-auto footercolor">
-                <h5>Contact Us</h5>
-                <div class="d-flex justify-content-center">
-                    <p class="pt-2 me-3">
-                        <i class="bi bi-envelope"></i> 
-                        <a href="mailto:mysticstar.ent@gmail.com">mysticstar.ent@gmail.com</a>
-                    </p>
-                    <p class="pt-2">
-                        <i class="bi bi-telephone"></i> 
-                        <a href="tel:+601159229221">+6011-59229221</a>
-                    </p>
-                </div>
-                <p class="small">&copy; Mystic Star Enterprise 2024</p>
-            </footer>
         </div>
+            
+        <footer class="site-footer text-center pt-3 mt-auto footercolor">
+            <h5>Contact Us</h5>
+            <div class="d-flex justify-content-center">
+                <p class="pt-2 me-3">
+                    <i class="bi bi-envelope"></i> 
+                    <a href="mailto:mysticstar.ent@gmail.com">mysticstar.ent@gmail.com</a>
+                </p>
+                <p class="pt-2">
+                    <i class="bi bi-telephone"></i> 
+                    <a href="tel:+601159229221">+6011-59229221</a>
+                </p>
+            </div>
+            <p class="small">&copy; Mystic Star Enterprise 2024</p>
+        </footer>
     </body>
 </html>
 
