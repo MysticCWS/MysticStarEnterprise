16336ebdf8ad4839bb349d1963b66ed9f69bc180
Terrence <Terrence@10.3.227.137>
2024-10-17 23:34:01 +0800

    Merge origin/master
    Conflicts:
    	includes/header2.php
    	index.php
    	signup.php
    
    # Conflicts:
    #	submitcartridge.php
    
    # Conflicts:
    #	home.php

--- home.php
+++ home.php
@@ -5,6 +5,14 @@ include 'includes\header.php';
 echo ' | Home';
 include 'includes\header2.php';
 include 'includes\navbar.php';
+
+//Fetch carousel from database
+$ref_table = 'carousel';
+$carousel_images = $database->getReference($ref_table)->getValue();
+
+//$carouselurlprefix = "https://firebasestorage.googleapis.com/v0/b/mysticstarenterprise.appspot.com/o/carousel%2F";
+//$carouselurlsuffix = "?alt=media";
+//$carouselurl = $carouselurlprefix.$carouselnum.$carouselurlsuffix;
 ?>
 
 <div class="content">
